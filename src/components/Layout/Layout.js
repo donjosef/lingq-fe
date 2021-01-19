6acd05aa976b4c15d002217ffdb8eb1ae6f29202
donjoe <giumontanaro@gmail.com>
2021-01-19 22:05:09 +0100

    Add a more specific class for search bar of layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ const Layout = (props) => {
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand text-info" href="#">LINGQ PRO</a>
 
-                <form className="form-inline">
+                <form className="form-inline navbar__form">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
                 </form>
