79e14531746f7c393151f4fbf4c531d123b2a7f3
donjoe <giumontanaro@gmail.com>
2021-01-19 22:07:16 +0100

    Whenever Select values change get new courses

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import Select from '../../components/Select/Select'
+import useSelect from '../../hooks/useSelect'
 import './Courses.css'
 
 const Courses = () => {
     const [courses, setCourses] = useState([])
     const [count, setCount] = useState(null)
 
+    const {langValue, levelValue, setLangValue, setLevelValue} = useSelect()
+
     useEffect(() => {
-        const lang = localStorage['lang']
-        const level = localStorage['level']
+        if(langValue) { //everytime langValue changes set new value on localstorage
+            localStorage.lang = langValue
+        }
+        if(levelValue) { //everytime levelValue changes set new value on localstorage
+            localStorage.level = levelValue
+        }
+        const lang = langValue || localStorage['lang']
+        const level = levelValue || localStorage['level']
 
         fetch(`http://localhost:4000/courses/${lang}/${level}`)
             .then(res => res.json())
@@ -15,10 +25,14 @@ const Courses = () => {
                 setCourses(courses.results)
                 setCount(courses.count)
             })
-    }, [])
+    }, [langValue, levelValue])
 
     return (
         <main className="container mt-5">
+            <form className="form-inline courses__controls">
+                <Select type="lang" value={langValue} onChange={setLangValue}/>
+                <Select type="level" value={levelValue} onChange={setLevelValue}/>
+            </form>
             <section className="row">
                 {courses.map(course => (
                     <div key={course.pk} className="card col-md-4 col-sm-6 mt-5">
