bad495ac30f2e1b8bca7d4cd4aed3f2124883fef
donjoe <giumontanaro@gmail.com>
2021-01-19 20:48:02 +0100

    Create component Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Layout.css'
+
+const Layout = (props) => {
+    return (
+        <>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand text-info" href="#">LINGQ PRO</a>
+
+                <form className="form-inline">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+            {props.children}
+        </>
+    )
+}
+
+export default Layout
